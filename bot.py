0ef6d23990cfa348c7f9f113834054a82ca5efb0
AmirCM <amirbehzadfar.h@gmail.com>
2020-11-14 01:23:21 +0330

    demo preparations

--- bot.py
+++ bot.py
@@ -1,97 +1,37 @@
+import logging
+from telegram import *
 from telegram.ext import *
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import Update
 
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-############################### Bot ############################################
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(main_menu_message(),
-                              reply_markup=main_menu_keyboard())
+logger = logging.getLogger(__name__)
 
-
-def main_menu(update: Update, context: CallbackContext):
-    query = update.callback_query
-    context.bot.edit_message_text(chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          text=main_menu_message(),
-                          reply_markup=main_menu_keyboard())
-
-
-def first_menu(update: Update, context: CallbackContext):
-    query = update.callback_query
-    context.bot.edit_message_text(chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          text=first_menu_message(),
-                          reply_markup=first_menu_keyboard())
-
-
-def second_menu(update: Update, context: CallbackContext):
-    query = update.callback_query
-    context.bot.edit_message_text(chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          text=second_menu_message(),
-                          reply_markup=second_menu_keyboard())
-
-
-# and so on for every callback_data option
-def first_submenu(bot, update):
-    pass
-
-
-def second_submenu(bot, update):
-    pass
-
-
-############################ Keyboards #########################################
-def main_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Option 1', callback_data='m1')],
-                [InlineKeyboardButton('Option 2', callback_data='m2')],
-                [InlineKeyboardButton('...', callback_data='m3')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def first_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
-                [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
-                [InlineKeyboardButton('Main menu', callback_data='main')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def second_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
-                [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
-                [InlineKeyboardButton('Main menu', callback_data='main')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-############################# Messages #########################################
-def main_menu_message():
-    return 'Choose the option in main menu:'
-
-
-def first_menu_message():
-    return 'Choose the submenu in first menu:'
-
-
-def second_menu_message():
-    return 'Choose the submenu in second menu:'
-
-
-############################# Handlers #########################################
 token = '1441929878:AAF7R_YIbI9y3hQdGyyeyWUv4LYELA0TOho'
 
 
-updater = Updater(token)
-dispatcher = updater.dispatcher
+def start(update: Update, context: CallbackContext):
+    """send hello msg"""
 
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
-dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
-dispatcher.add_handler(CallbackQueryHandler(first_submenu,
-                                            pattern='m1_1'))
-dispatcher.add_handler(CallbackQueryHandler(second_submenu,
-                                            pattern='m2_1'))
+    print(context)
+    print(update)
+    update.message.delete()
+    with open('hello.txt', mode='r', encoding='UTF-8') as file:
+        txt = file.read()
+    context.bot.send_message(update.message.chat_id, txt)
 
-updater.start_polling()
-################################################################################
+
+def bot():
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    bot()
--- main.py
+++ main.py
@@ -1,63 +1,187 @@
 import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    ConversationHandler,
+    CallbackContext,
+)
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
-
+# Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
+
 logger = logging.getLogger(__name__)
 
+# Stages
+FIRST, SECOND, THIRD = range(3)
+# Callback data
+ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN = range(7)
+dollar, euro, pond, yuan, leer = range(10, 15)
+HAVALEH, DIGITAL = range(2)
+currency = {'Bitcoin': 'BTC',
+            'Ethereum': 'ETH',
+            'Monero': 'XMR',
+            'Dash': 'DASH',
+            'Litecoin': 'LTC',
+            'Tether': 'USDT',
+            'Cardano': 'ADA',
+            'TRON': 'TRX'}
 
-class Money_Bot:
-    def __init__(self):
-        self.keyboard = [
-            [
-                InlineKeyboardButton("ارز حواله", callback_data='1'),
-                InlineKeyboardButton("ارز دیجیتال", callback_data='2'),
-            ],
-            [InlineKeyboardButton("HELP", callback_data='3')],
+
+def start(update: Update, context: CallbackContext) -> None:
+    print('start')
+    # Get user that sent /start and log his name
+    user = update.message.from_user
+    print("User {} started the conversation.".format(user.first_name))
+    # Build InlineKeyboard where each button has a displayed text
+    # and a string as callback_data
+    # The keyboard is a list of button rows, where each row is in turn
+    # a list (hence `[[...]]`).
+    keyboard = [
+        [
+            InlineKeyboardButton("ارز حواله", callback_data=str(HAVALEH)),
+            InlineKeyboardButton("ارز دیجیتال", callback_data=str(DIGITAL)),
         ]
-
-    def start(self, update: Update, context: CallbackContext) -> None:
-        reply_markup = InlineKeyboardMarkup(self.keyboard)
-
-        update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-    def button(update: Update, context: CallbackContext) -> None:
-        query = update.callback_query
-
-        # CallbackQueries need to be answered, even if no notification to the user is needed
-        # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-        query.answer()
-
-        query.edit_message_text(text="Selected option: {}".format(query.data))
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    # Send message with text and appended InlineKeyboard
+    update.message.reply_text("معامله خود را شروع کنید", reply_markup=reply_markup)
+    # Tell ConversationHandler that we're in state `FIRST` now
+    return FIRST
 
 
-    def help_command(update: Update, context: CallbackContext) -> None:
-        update.message.reply_text("Use /start to test this bot.")
+def start_over(update: Update, context: CallbackContext) -> None:
+    print('start over')
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("ارز حواله", callback_data=str(HAVALEH)),
+            InlineKeyboardButton("ارز دیجیتال", callback_data=str(DIGITAL)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    query.edit_message_text(text="معامله خود را شروع کنید", reply_markup=reply_markup)
+    return FIRST
 
 
-    def run(self):
-        # Create the Updater and pass it your bot's token.
-        # Make sure to set use_context=True to use the new context based callbacks
-        # Post version 12 this will no longer be necessary
-        token = '1441929878:AAF7R_YIbI9y3hQdGyyeyWUv4LYELA0TOho'
-        updater = Updater(token, use_context=True)
+def havaleh(update: Update, context: CallbackContext) -> None:
+    print('ARZ HAVALEH')
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("دلار", callback_data=str(dollar)),
+            InlineKeyboardButton("یورو", callback_data=str(euro)),
+            InlineKeyboardButton("پوند", callback_data=str(pond))
+        ],
+        [
+            InlineKeyboardButton("یوان", callback_data=str(yuan)),
+            InlineKeyboardButton("لیر", callback_data=str(leer))
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="ارز حواله", reply_markup=reply_markup
+    )
+    return SECOND
 
-        updater.dispatcher.add_handler(CommandHandler('start', self.start))
-        updater.dispatcher.add_handler(CallbackQueryHandler(self.button))
-        updater.dispatcher.add_handler(CommandHandler('help', self.help_command))
 
-        # Start the Bot
-        updater.start_polling()
+def digital(update: Update, context: CallbackContext) -> None:
+    print('ARZ DIGITAL')
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
 
-        # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-        # SIGTERM or SIGABRT
-        updater.idle()
+    keyboard = [
+        [
+            InlineKeyboardButton("Bitcoin", callback_data=str(currency['Bitcoin'])),
+            InlineKeyboardButton("Ethereum", callback_data=str(currency['Ethereum'])),
+            InlineKeyboardButton("Monero", callback_data=str(currency['Monero'])),
+        ],
+        [
+            InlineKeyboardButton("Dash", callback_data=str(currency['Dash'])),
+            InlineKeyboardButton("Litecoin", callback_data=str(currency['Litecoin'])),
+            InlineKeyboardButton("Tether", callback_data=str(currency['Tether'])),
+        ],
+        [
+            InlineKeyboardButton("Cardano", callback_data=str(currency['Cardano'])),
+            InlineKeyboardButton('TRON', callback_data=str(currency['TRON']))
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="ارز دیجیتال", reply_markup=reply_markup
+    )
+    return SECOND
+
+
+def other(update: Update, context: CallbackContext) -> None:
+    print('other')
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    
+    keyboard = [
+        [
+            InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
+            InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Third CallbackQueryHandler. Do want to start over?", reply_markup=reply_markup
+    )
+    # Transfer to conversation state `SECOND`
+    return THIRD
+
+
+def end(update: Update, context: CallbackContext) -> None:
+    print('END')
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="See you next time!")
+    return ConversationHandler.END
+
+
+def main():
+    updater = Updater("1441929878:AAF7R_YIbI9y3hQdGyyeyWUv4LYELA0TOho")
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            FIRST: [
+                CallbackQueryHandler(havaleh, pattern='^' + str(HAVALEH) + '$'),
+                CallbackQueryHandler(digital, pattern='^' + str(DIGITAL) + '$'),
+            ],
+            SECOND: [
+                CallbackQueryHandler(other, pattern='^\d*$')
+            ],
+            THIRD: [
+                CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+                CallbackQueryHandler(end, pattern='^' + str(TWO) + '$'),
+            ],
+        },
+        fallbacks=[CommandHandler('start', start)],
+    )
+
+    # Add ConversationHandler to dispatcher that will be used for handling
+    # updates
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
 if __name__ == '__main__':
-    bot = Money_Bot()
-    bot.run()
+    main()
