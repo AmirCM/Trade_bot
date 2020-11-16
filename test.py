b73539c48a0e978c955f4be06d6319af8b280f81
AmirCM <amirbehzadfar.h@gmail.com>
2020-11-16 15:03:23 +0330

    DBMS updated

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import sqlite3 as sql
+from user import User
 
 conn = sql.connect('users.db')
 cur = conn.cursor()
-cur.execute("""CREATE TABLE users(
-              id TEXT PRIMARY KEY,
-              phone TEXT,
-              is_auth INTEGER,
-              is_joined INTEGER
-          )""")
-
-conn.commit()
-conn.close()
+sar = User('amir_ahb', '09387151079')
+sar.is_joined = True
+with conn:
+    cur.execute("INSERT INTO users VALUES (?, ?, ?, ?)", sar.get_data())
+print(sar.get_data(), type(sar.get_data()))
+cur.execute("SELECT * FROM users")
+print(cur.fetchall())
--- user.py
+++ user.py
@@ -4,3 +4,6 @@ class User:
         self.phone = phone
         self.is_auth = False
         self.is_joined = False
+
+    def get_data(self):
+        return self.username, self.phone, self.is_auth, self.is_joined
